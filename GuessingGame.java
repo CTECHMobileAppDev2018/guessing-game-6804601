25230814a7be8714dc723daf0743ea15c85eaa68
6804601 <6806401@isd535.org>
2019-02-19 09:07:25 -0600

    Completed Guessing Game
    implimented all of the working parts of the code so that it works seamlessly

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,10 +1,28 @@
-
+import java.util.Scanner;
 public class GuessingGame {
-
-    public static void main(String[] args) {
+  
+  public static void main(String[] args) {
     
-        System.out.println("Welcome to the guessing game.");
-    
+    Scanner scan = new Scanner(System.in);
+    int numGuess = 0, playerGuess = -1,done = 0, numTries = 0;
+    int gameGuess = (int)(Math.random()*99+1);
+    System.out.println("Welcome to the guessing game.");
+    while(done == 0){
+      System.out.println("Please guess a number between 1-100.");
+      playerGuess = scan.nextInt();
+      numGuess++;
+      
+      if(playerGuess == gameGuess){
+        System.out.println("Correct! Your guess was: "+playerGuess+", it took you "+numTries+" tries.");
+        done++;
+      }
+      else if(playerGuess < gameGuess){
+        System.out.println("That guess was too low");
+      }
+      else if(playerGuess > gameGuess){
+        System.out.println("That guess was too high");
+      }
     }
-
+    
+  }
 }
